341b6822bebf52edd263d87564091f22816ca53a
Your Name <you@example.com>
2024-03-25 12:12:57 +0000

    wip

--- src/train.py
+++ src/train.py
@@ -116,24 +116,161 @@ class Agent(nn.Module):
         return action, probs.log_prob(action), probs.entropy(), self.critic(hidden), lstm_state
 
 
-num_envs = 16
-device = torch.device("cuda" if torch.cuda.is_available()
-                      and args.cuda else "cpu")
-env = gym.vector.SyncVectorEnv([make_env] * num_envs)
-agent = Agent(num_actions=len(positions)).to(device)
-optimizer = optim.Adam(agent.parameters(), lr=2.5e-4, eps=1e-5)
+if __name__ = "__main__":
+    num_envs = 16
+    T = 200
+    device = torch.device("cuda" if torch.cuda.is_available()
+                          and args.cuda else "cpu")
+    env = gym.vector.SyncVectorEnv([make_env] * num_envs)
+    agent = Agent(num_actions=len(positions)).to(device)
+    optimizer = optim.Adam(agent.parameters(), lr=2.5e-4, eps=1e-5)
 
-observation, info = env.reset()
-print(observation.shape)
+    next_obs, info = env.reset()
+    print(observation.shape)
 
-pbar = tqdm(total=1000*1000)
-while True:
-    action = env.action_space.sample()
-    observation, reward, done, truncated, info = env.step(action)
-    pbar.update(num_envs)
+    obs = torch.zeros((T, *observation.shape)).to(device)
+    actions = torch.zeros((T, num_envs)).to(device)
+    logprobs = torch.zeros((T, num_envs)).to(device)
+    rewards = torch.zeros((T, num_envs)).to(device)
+    dones = torch.zeros((T, num_envs)).to(device)
+    values = torch.zeros((T, num_envs)).to(device)
+
+    next_done = torch.zeros(num_envs).to(device)
+    next_lstm_state = (
+        torch.zeros(agent.lstm.num_layers, num_envs,
+                    agent.lstm.hidden_size).to(device),
+        torch.zeros(agent.lstm.num_layers, num_envs,
+                    agent.lstm.hidden_size).to(device),
+    )  # hidden and cell states (see https://youtu.be/8HyCNIVRbSU)
+
+    while True:
+        initial_lstm_state = (
+            next_lstm_state[0].clone(), next_lstm_state[1].clone())
+
+        for step in range(0, T):
+            obs[step] = next_obs
+            dones[step] = next_done
+            with torch.no_grad():
+                action, logprob, _, value, next_lstm_state = agent.get_action_and_value(
+                    next_obs, next_lstm_state, next_done)
+                values[step] = value.flatten()
+            actions[step] = action
+            logprobs[step] = logprob
+
+            next_obs, reward, terminations, truncations, infos = envs.step(
+                action.cpu().numpy())
+            next_done = np.logical_or(terminations, truncations)
+            rewards[step] = torch.tensor(reward).to(device).view(-1)
+            next_obs, next_done = torch.Tensor(next_obs).to(
+                device), torch.Tensor(next_done).to(device)
+
+        # bootstrap value if not done
+        with torch.no_grad():
+            next_value = agent.get_value(
+                next_obs,
+                next_lstm_state,
+                next_done,
+            ).reshape(1, -1)
+            advantages = torch.zeros_like(rewards).to(device)
+            lastgaelam = 0
+            for t in reversed(range(args.num_steps)):
+                if t == args.num_steps - 1:
+                    nextnonterminal = 1.0 - next_done
+                    nextvalues = next_value
+                else:
+                    nextnonterminal = 1.0 - dones[t + 1]
+                    nextvalues = values[t + 1]
+                delta = rewards[t] + args.gamma * \
+                    nextvalues * nextnonterminal - values[t]
+                advantages[t] = lastgaelam = delta + args.gamma * \
+                    args.gae_lambda * nextnonterminal * lastgaelam
+            returns = advantages + values
+
+        # flatten the batch
+        b_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
+        b_logprobs = logprobs.reshape(-1)
+        b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
+        b_dones = dones.reshape(-1)
+        b_advantages = advantages.reshape(-1)
+        b_returns = returns.reshape(-1)
+        b_values = values.reshape(-1)
+
+        # Optimizing the policy and value network
+
+        # Optimizing the policy and value network
+        assert args.num_envs % args.num_minibatches == 0
+        envsperbatch = args.num_envs // args.num_minibatches
+        envinds = np.arange(args.num_envs)
+        flatinds = np.arange(args.batch_size).reshape(
+            args.num_steps, args.num_envs)
+        clipfracs = []
+        for epoch in range(args.update_epochs):
+            np.random.shuffle(envinds)
+            for start in range(0, args.num_envs, envsperbatch):
+                end = start + envsperbatch
+                mbenvinds = envinds[start:end]
+                # be really careful about the index
+                mb_inds = flatinds[:, mbenvinds].ravel()
+
+                _, newlogprob, entropy, newvalue, _ = agent.get_action_and_value(
+                    b_obs[mb_inds],
+                    (initial_lstm_state[0][:, mbenvinds],
+                     initial_lstm_state[1][:, mbenvinds]),
+                    b_dones[mb_inds],
+                    b_actions.long()[mb_inds],
+                )
+                logratio = newlogprob - b_logprobs[mb_inds]
+                ratio = logratio.exp()
+
+                with torch.no_grad():
+                    # calculate approx_kl http://joschu.net/blog/kl-approx.html
+                    old_approx_kl = (-logratio).mean()
+                    approx_kl = ((ratio - 1) - logratio).mean()
+                    clipfracs += [((ratio - 1.0).abs() >
+                                   args.clip_coef).float().mean().item()]
+
+                mb_advantages = b_advantages[mb_inds]
+                if args.norm_adv:
+                    mb_advantages = (
+                        mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
+
+                # Policy loss
+                pg_loss1 = -mb_advantages * ratio
+                pg_loss2 = -mb_advantages * \
+                    torch.clamp(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
+                pg_loss = torch.max(pg_loss1, pg_loss2).mean()
+
+                # Value loss
+                newvalue = newvalue.view(-1)
+                if args.clip_vloss:
+                    v_loss_unclipped = (newvalue - b_returns[mb_inds]) ** 2
+                    v_clipped = b_values[mb_inds] + torch.clamp(
+                        newvalue - b_values[mb_inds],
+                        -args.clip_coef,
+                        args.clip_coef,
+                    )
+                    v_loss_clipped = (v_clipped - b_returns[mb_inds]) ** 2
+                    v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
+                    v_loss = 0.5 * v_loss_max.mean()
+                else:
+                    v_loss = 0.5 * \
+                        ((newvalue - b_returns[mb_inds]) ** 2).mean()
+
+                entropy_loss = entropy.mean()
+                loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef
+
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(
+                    agent.parameters(), args.max_grad_norm)
+                optimizer.step()
+
+        y_pred, y_true = b_values.cpu().numpy(), b_returns.cpu().numpy()
+        var_y = np.var(y_true)
+        explained_var = np.nan if var_y == 0 else 1 - \
+            np.var(y_true - y_pred) / var_y
 
 
-#
 # env.save_for_render()
 # renderer = Renderer(render_logs_dir="render_logs")
 # renderer.run()
