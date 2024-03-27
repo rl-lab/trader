0db5ab73210443d76ca3f214a9de7fa52f9c94e8
Your Name <you@example.com>
2024-03-27 08:47:39 +0000

    wip

--- scripts/debug.py
+++ scripts/debug.py
@@ -1,10 +1,39 @@
 import sys
+import random
 
 import pickle
 import numpy as np
+import pandas as pd
+from glob import glob
+import random
+
+
+def preprocess_function(df):
+    df.sort_index(inplace=True)
+    df.drop_duplicates(inplace=True)
+    df["feature_close"] = df["close"].pct_change()
+    df["feature_open"] = df["open"] / df["close"]
+    df["feature_high"] = df["high"] / df["close"]
+    df["feature_low"] = df["low"] / df["close"]
+    df["feature_volume"] = df["amount"].pct_change()
+    df.dropna(inplace=True)
+    df = df[~df.isin([np.inf, -np.inf]).any(axis=1)]
+    return df
+
 
 if __name__ == "__main__":
     sys.path.append("build")
     import trade_env
-    vec_trade = trade_env.VecTrade(7)
+    vec_trade = trade_env.VecTrade(5, 128)
+    instruments = glob("data/*.csv.gz")
+    ins = random.choice(instruments)
+    df = preprocess_function(pd.read_csv(
+        ins, parse_dates=["time"], index_col="time"))
+
+    code = df["code"][0]
+    raw_names = ['open', 'high', 'low', 'close', 'volume']
+    fea_names = ['feature_open', 'feature_high',
+                 'feature_low', 'feature_close', 'feature_volume']
+
+    vec_trade.Load(code, df[raw_names].values, df[fea_names].values)
 
--- src/envapi.cpp
+++ src/envapi.cpp
@@ -5,25 +5,48 @@
 
 namespace py = pybind11;
 
+using feature_t = std::pair<std::vector<float>, std::vector<float>>;
+
 class vec_trade_t {
 public:
-  vec_trade_t(int n_feature) : n_feature_(n_feature) {}
+  vec_trade_t(int n_feature, int bs) : n_feature_(n_feature), bs_(bs) {}
   void Load(std::string name,
-            py::array_t<float, py::array::c_style | py::array::forcecast> arr) {
+            py::array_t<float, py::array::c_style | py::array::forcecast> raw,
+            py::array_t<float, py::array::c_style | py::array::forcecast> fea) {
 
-    int64_t n = arr.size();
-    ctx[name] = std::vector<float>(n);
-    std::memcpy(ctx[name].data(), arr.data(), n * sizeof(float));
+    int64_t n = raw.size();
+    int64_t m = raw.size();
+    ctx[name] = {std::vector<float>(n), std::vector<float>(m)};
+    std::memcpy(ctx[name].first.data(), raw.data(), n * sizeof(float));
+    std::memcpy(ctx[name].second.data(), fea.data(), m * sizeof(float));
+  };
+
+  py::array_t<float> /*obs*/ Reset() {
+    std::vector<float> obs;
+    return py::array({bs_, n_feature_}, obs.data());
+  };
+
+  std::tuple<py::array_t<float>, py::array_t<float>, py::array_t<int>>
+  Step(std::vector<int> actions) {
+    std::vector<float> obs;
+    std::vector<float> reward;
+    std::vector<int> done;
+
+    return {py::array({bs_, n_feature_}, obs.data()),
+            py::array({bs_}, reward.data()), py::array({bs_}, done.data())};
   }
 
   int n_feature_;
-  std::map<std::string, std::vector<float>> ctx;
+  int bs_;
+  std::map<std::string, feature_t> ctx;
 };
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(trade_env, m) {
   py::class_<vec_trade_t>(m, "VecTrade")
-      .def(py::init<int>())
+      .def(py::init<int, int>())
+      .def("Reset", &vec_trade_t::Reset)
+      .def("Step", &vec_trade_t::Step)
       .def("Load", &vec_trade_t::Load);
 }
