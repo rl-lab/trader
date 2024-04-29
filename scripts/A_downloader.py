e6c2f5a1ead4a296e9bd09a520cfa0389e991df1
yuchen <chenyu414@gmail.com>
2024-04-30 00:56:52 +0800

    wip

--- scripts/A_downloader.py
+++ scripts/A_downloader.py
@@ -10,18 +10,20 @@ stocks = []
 while (rs.error_code == '0') & rs.next():
     stocks.append(rs.get_row_data())
 
+dayq = "date,code,open,high,low,close,amount,turn,peTTM,pbMRQ,psTTM,pcfNcfTTM,isST",
+minq = "date,time,code,open,high,low,close,amount"
+
 for _, code, *args in tqdm(stocks):
     rs = bs.query_history_k_data_plus(
-        code,
-        "date,code,open,high,low,close,amount,turn,peTTM,pbMRQ,psTTM,pcfNcfTTM,isST",
-        start_date='2024-01-01', end_date='2024-12-31',
-        frequency="d", adjustflag="1")
+        code, minq,
+        start_date='2000-01-01', end_date='2024-12-31',
+        frequency="30", adjustflag="1")
 
     data_list = []
     while (rs.error_code == '0') & rs.next():
         data_list.append(rs.get_row_data())
     result = pd.DataFrame(data_list, columns=rs.fields)
 
-    result.to_csv(f"data/{code}.csv.gz", compression='gzip', index=False)
+    result.to_csv(f"data/{code}.30min.csv.gz", compression='gzip', index=False)
 
 bs.logout()
